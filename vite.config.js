dcf5ed30ea4d9db28589e591df23f46f57829fdd
Jhonnatan Zevallos <jhonna7an@gmail.com>
2024-06-23 16:27:09 -0300

    vite config updated

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    sourcemap: false, // Desactiva la generación de sourcemaps
+    minify: false, // Desactiva la minificación para reducir el uso de memoria
+    rollupOptions: {
+      // Añadir más opciones de configuración de Rollup si es necesario
+    },
+  }
 })
